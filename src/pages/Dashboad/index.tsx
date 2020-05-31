e74d4b13e1a0ce7be68b8b0e3daeb9ff1e058865
Valdir Coxev [Home] <valdircoxevdev@gmail.com>
2020-05-31 05:00:34 -0400

    Navegando entre rotas

--- src/pages/Dashboad/index.tsx
+++ src/pages/Dashboad/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
 import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 
 import { FiChevronRight } from 'react-icons/fi';
 import { Title, Form, InputError, Repository } from './styles';
@@ -72,14 +73,17 @@ const Dashboard: React.FC = () => {
       {inputError && <InputError>{inputError}</InputError>}
       <Repository>
         {repositories.map((repository) => (
-          <a href="#reactjs" key={repository.full_name}>
+          <Link
+            to={`repository/${repository.full_name}`}
+            key={repository.full_name}
+          >
             <img src={repository.owner.avatar_url} alt={repository.full_name} />
             <div>
               <strong>{repository.full_name}</strong>
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repository>
     </>
